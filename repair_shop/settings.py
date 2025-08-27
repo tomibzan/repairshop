b8389110d43017f0a4e78bb762f9b9b1fed83947
tomibzan <thomflowen@gmail.com>
2025-08-27 21:55:00 +0300

    Revert "Initial commit to Imroved workshop management app"
    This reverts commit a438b0373f1119651969c76243bb1f21a8e51368.

--- repair_shop/settings.py
+++ repair_shop/settings.py
@@ -75,8 +75,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
--- repair_shop/urls.py
+++ repair_shop/urls.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from workshop.admin import custom_admin_site
 
 urlpatterns = [
-    path("admin/", custom_admin_site.urls),
+    path("admin/", admin.site.urls),
     path("", include(("workshop.urls", "workshop"), namespace="workshop")),  # landing & other URLs
 ]
 
